16cec3118dcf74639ea8d87190c8b0aa6739ab1c
Mike_Smith2 <MikeSmith9107@gmail.com>
2021-04-19 08:28:31 -0400

    Changed from csv to read from database

--- src/client/readPartNumbers.py
+++ src/client/readPartNumbers.py
@@ -3,12 +3,15 @@ import sys
 import os
 import urllib.request
 import urllib.error
+import query_part_picker_db
+
 sys.path.insert(0, (os.path.abspath('../misc')))
 
 import primitives
 import os
 
 _primitives = primitives.Primitives("Client", "Debug")
+
 
 def download_racks_csv(url):
     response = urllib.request.urlopen(url)
@@ -17,16 +20,17 @@ def download_racks_csv(url):
     racks_file = open("Racks.csv", "w")
     racks_file.write(text)
 
-def find_my_parts(local_ip, path_to_client=None):
+
+def find_my_parts(local_ip, directory_server, path_to_client=None):
     """Given a nodes static IP, find all part numbers assigned to it in the master spreadsheet
         Returns list [(part number, part name, line #), ..., (part number n, part name n, line # n)]"""
     if path_to_client:
         try:
             os.chdir(path_to_client)
         except FileNotFoundError:
-            print("Directory does't exist: "+str(path_to_client))
+            print("Directory does't exist: " + str(path_to_client))
             return
-          
+
     our_parts = []
 
     ip_bytes = local_ip.split('.')
@@ -34,15 +38,20 @@ def find_my_parts(local_ip, path_to_client=None):
 
     _primitives.log("Fetching part numbers for " + local_ip + "...", in_log_level="Debug")
 
-    try:
-        download_racks_csv('http://73.17.34.121/hosted/Racks.csv')
-        part_number_assignments = open("Racks.csv")
+    """try:
+        racks_csv_text = _primitives.download_file(directory_server + "Racks.csv")
+
+        if racks_csv_text != 1:
+            open(os.path.abspath("./Racks.csv"), "w").write(racks_csv_text)
+            part_number_assignments = open("./Racks.csv")
+        else:
+            raise urllib.error.URLError("Could not access Racks.csv. Directory server offline?")
 
     except urllib.error.URLError:
         print("ERROR: No internet connection detected; cannot download Racks file... Searching for local copy...")
 
         try:
-            part_number_assignments = open("Racks.csv")
+            part_number_assignments = open(os.path.abspath("./Racks.csv"))
             print("Local Racks.csv found! Proceeding...")
 
         except FileNotFoundError:
@@ -50,13 +59,25 @@ def find_my_parts(local_ip, path_to_client=None):
             return []
 
     csv_reader = csv.reader(part_number_assignments, delimiter=',')
+    """
+    for row in query_part_picker_db.query_db():
+        if ip_byte_four in row[3]:
+            our_parts.append((row[0], row[1], row[2], row[4]))
+
+    _primitives.log("Found " + str(len(our_parts)) + " parts assigned to " + local_ip + "...", in_log_level='Debug')
+
+    return our_parts
+
+
+if __name__ == "__main__":
+    print(find_my_parts(_primitives.get_local_ip()))
+"""
     for row in csv_reader:
+        # TODO Search database
         if ip_byte_four in row[3]:
             our_parts.append((row[0], row[1], row[4]))
 
     _primitives.log("Found "+str(len(our_parts)) + " parts assigned to "+local_ip+"...", in_log_level='Debug')
 
     return our_parts
-
-if __name__ == "__main__":
-    print(find_my_parts(_primitives.get_local_ip()))
+"""
